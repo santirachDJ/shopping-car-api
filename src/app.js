2c88f853bc52f96636ce1e7fe54c887c28895da1
santiagoDJ <santiago.martinez@condorlabs.io>
2022-03-30 14:24:11 -0500

    add dependecies requerid condor labs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
-
+import { healthMonitor } from '@condor-labs/health-middleware';
 // App
 const app = express();
+healthMonitor(app);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
